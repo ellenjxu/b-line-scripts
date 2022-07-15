d62345391c78c26574db0c5f0961130289812214
Ellen Xu <xu.ellen@outlook.com>
2022-07-15 16:07:17 -0400

    test threshold in pr curve

--- results/precision_recall.py
+++ results/precision_recall.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-from sklearn.metrics import average_precision_score, precision_recall_curve, PrecisionRecallDisplay, plot_precision_recall_curve
+from sklearn.metrics import average_precision_score, precision_recall_curve, PrecisionRecallDisplay, precision_score, recall_score
 
 # load the data
 df = pd.read_csv('C:/Users/ellen/Documents/code/B-line_detection/scripts/results/all_labels.csv')
@@ -8,6 +8,20 @@ df = df[['chosen_answer', 'crowd_label', 'expert_chosen_answer', 'gs_label']]
 # print(df.head())
 
 classification = ['no b-lines', '1 or more discrete b-lines', 'confluent b-lines']
+
+# ------------------------------------------------------------ Test ------------------------------------------------------------
+
+def test_threshold(thresh, y_labels, y_hat_crowd, y_hat_expert):
+    y_hat_crowd_thresh = [1 if y_hat_crowd[i] >= thresh else 0 for i in range(len(y_hat_crowd))]
+    y_hat_expert_thresh = [1 if y_hat_expert[i] >= thresh else 0 for i in range(len(y_hat_expert))]
+
+    crowd_precision = precision_score(y_labels, y_hat_crowd_thresh)
+    crowd_recall = recall_score(y_labels, y_hat_crowd_thresh)
+
+    expert_precision = precision_score(y_labels, y_hat_expert_thresh)
+    expert_recall = recall_score(y_labels, y_hat_expert_thresh)
+
+    return (crowd_precision, crowd_recall), (expert_precision, expert_recall)
 
 # ------------------------------------------------------------ PR curves ----------------------------------------------------------------------
 
@@ -46,6 +60,10 @@ for classes in classification:
     ap_crowd = average_precision_score(y_labels, y_hat_crowd_probs)
     ap_expert = average_precision_score(y_labels, y_hat_expert_probs)
 
+    # test
+
+    print(test_threshold(0.5, y_labels, y_hat_crowd_probs, y_hat_expert_probs))
+
     # precision and recall curve
 
     precision, recall, _ = precision_recall_curve(y_labels, y_hat_crowd_probs)
@@ -57,4 +75,5 @@ for classes in classification:
     plt.title(f'Precision-Recall curve for {classes}')
     plt.show()
 
-df.to_csv('C:/Users/ellen/Documents/code/B-line_detection/scripts/results/precision_recall_out.csv', index=False)
+# df.to_csv('C:/Users/ellen/Documents/code/B-line_detection/scripts/results/precision_recall_out.csv', index=False)
+# print(df["expert_score_no b-lines"].value_counts())

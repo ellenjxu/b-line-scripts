63a14136a24b2621931d60e8f16a0622a8197d9b
Ellen Xu <xu.ellen@outlook.com>
2022-07-25 14:57:39 -0400

    ai pseudo labels on all training data

--- pseudo_labels/read_pkl.py
+++ pseudo_labels/read_pkl.py
@@ -4,9 +4,12 @@ from natsort import natsorted
 split_file = "C:/Users/ellen/Documents/code/B-line_detection/intermediate/data/info/dataset_split_dictionary.pkl"
 frame_file = "C:/Users/ellen/Documents/code/B-line_detection/intermediate/data/info/frames_dictionary.pkl"
 
-with open(frame_file, 'rb') as f:
-    data = pickle.load(f)
+# with open(frame_file, 'rb') as f:
+#     data = pickle.load(f)
 
+# print(data["Case-112"])
+# print(natsorted(data))
 
-print(data["Case-112"])
-print(natsorted(data))
+import pandas as pd
+split_dict = pd.read_pickle(split_file)
+print(split_dict['test'])
